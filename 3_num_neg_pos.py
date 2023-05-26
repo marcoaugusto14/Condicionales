a3d250a397b1203f4381d65fd57303e5460c0afd
unknown <marcoaugusto1410@hotmail.com>
2023-05-26 15:22:36 -0500

    Escriba un programa para verificar si un número es negativo, positivo o cero

---
+++ 3_num_neg_pos.py
@@ -0,0 +1,10 @@
+#Escriba un programa para verificar si un número es negativo, positivo o cero.
+
+a=int(input("Ingrese un nùmero "))
+
+if a<0:
+  print("El nùmero es negativo")
+elif a>0:
+  print("El nùmero es positivo")
+else:
+  print("El nùmero es cero")
