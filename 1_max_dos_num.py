e35cc92e4f2bc52e78857edd1dd1da1464704819
unknown <marcoaugusto1410@hotmail.com>
2023-05-26 14:32:16 -0500

    Escriba un programa para encontrar el máximo entre dos números

---
+++ 1_max_dos_num.py
@@ -0,0 +1,11 @@
+#Escriba un programa para encontrar el máximo entre dos números.
+
+a=input("Ingrese el primer nùmero ")
+b=input("Ingrese el segundo nùmero ")
+
+if a>b:
+  print(a, "es mayor a ", b)
+elif a<b:
+  print(a, "es menor a ", b)
+else:
+  print("Los nùmeros son iguales")
